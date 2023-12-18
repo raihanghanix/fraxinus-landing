a2202e1c3b48461fa5fce53274382330bad7d40a
unknown <raihanghani58@gmail.com>
2023-12-18 09:56:50 +0700

    fix

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import { NextResponse } from "next/server";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
@@ -10,8 +11,8 @@ export const GET = async (req) => {
     const { data: queryData, error: queryError } = await supabase
       .from("posts")
       .select();
-    return new Response(JSON.stringify(queryData), { status: 200 });
+    return new NextResponse(JSON.stringify(queryData), { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify(error), { status: 500 });
+    return new NextResponse(JSON.stringify(error), { status: 500 });
   }
 };
